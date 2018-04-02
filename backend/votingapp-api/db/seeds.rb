7f04f6c381d1822b3297a8e56d77994f3b8e660e
jrmart12 <rodolfojose@live.com>
2018-04-01 22:41:02 -0600

    seeds

--- backend/votingapp-api/db/seeds.rb
+++ backend/votingapp-api/db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.create(
+ [
+    {
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: 55,
+
+      submitterAvatarUrl: 'images/avatars/daniel.jpg',
+      productImageUrl: 'images/products/image-aqua.png',
+    },
+    {
+      title: 'Supermajority: The Fantasy Congress League',
+      description: 'Earn points when your favorite politicians pass legislation.',
+      url: '#',
+      votes: 53,
+
+      submitterAvatarUrl: 'images/avatars/kristy.png',
+      productImageUrl: 'images/products/image-rose.png',
+    },
+    {
+      title: 'Tinfoild: Tailored tinfoil hats',
+      description: 'We already have your measurements and shipping address.',
+      url: '#',
+
+      votes: 12,
+      submitterAvatarUrl: 'images/avatars/veronika.jpg',
+      productImageUrl: 'images/products/image-steel.png',
+    },
+    {
+      title: 'Haught or Naught',
+      description: 'High-minded or absent-minded? You decide.',
+      url: '#',
+      votes: 9,
+      
+      submitterAvatarUrl: 'images/avatars/molly.png',
+      productImageUrl: 'images/products/image-yellow.png',
+    },
+  ]
+  )
