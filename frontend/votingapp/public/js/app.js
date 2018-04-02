f6a959d0d47ae5b67f1e1a84df24ae00e2f4624e
jrmart12 <rodolfojose@live.com>
2018-04-01 23:07:13 -0600

    react component

--- frontend/votingapp/public/js/app.js
+++ frontend/votingapp/public/js/app.js
@@ -1,49 +1,19 @@
-class ProductList extends React.Component {
-  state = {
-    products: [],
-    };
+import React, { Component } from 'react';
+import './App.css';
+import ProductsContainer from './components/ProductsContainer'
 
-  componentDidMount() {
-    this.setState({ products: Seed.products });
-  }
-
-  handleProductUpVote = (productId) =>{ //se crea un nuevo array de productos y si se modifica, se modifica un clone envez del original
-    const nextProducts = this.state.products.map((product) => {
-      if (product.id === productId) {
-        return Object.assign({}, product, {
-          votes: product.votes + 1,
-        });
-      } else {
-        return product;
-        }
-      });
-      this.setState({
-      products: nextProducts,
-    });
-  }
-
+class App extends Component {
   render() {
-    const products = this.state.products.sort((a, b) => (
-      b.votes - a.votes
-      ));
-      const productComponents = products.map((product) => (
-        <Product
-          key={'product-' + product.id}
-          id={product.id}
-          title={product.title}
-          description={product.description}
-          url={product.url}
-          votes={product.votes}
-          submitterAvatarUrl={product.submitterAvatarUrl}
-          productImageUrl={product.productImageUrl}
-          onVote={this.handleProductUpVote}
-        />
-    ));
     return (
-      <div className='ui unstackable items'>
-        {productComponents}
+      <div className="App">
+        <div className="App-header">
+          <h1>Popular Products</h1>
+        </div>
+        <ProductsContainer />
       </div>
     );
   }
 }
+
+export default App;
 
