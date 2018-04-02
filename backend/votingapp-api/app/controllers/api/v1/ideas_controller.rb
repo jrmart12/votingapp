a0411f7a6d6da1edd6b66df09e2e6b6508a3172d
jrmart12 <rodolfojose@live.com>
2018-04-01 22:47:20 -0600

    product controller

---
+++ backend/votingapp-api/app/controllers/api/v1/ideas_controller.rb
@@ -0,0 +1,35 @@
+module Api::V1
+  class IdeasController < ApplicationController
+        def index
+			@products = Product.all
+			render json: @products 
+		end
+
+		def create
+			@product = Product.create(product_params)
+			render json: @product
+		end
+
+		def update
+			@product = Product.find(params[:id])
+			@product.update_attributes(product_params)
+			render json: @product
+		end
+
+		def destroy
+			@product = Product.find(params[:id])
+			if @product.destroy
+			  head :no_content, status: :ok
+			else
+			  render json: @prodcut.errors, status: :unprocessable_entity
+			end
+		  end
+
+		private
+
+		def product_params
+			params.require(:product).permit(:title, :description, :votes, :submitterAvatarUrl,:productImageUrl)
+		end
+
+    end
+end
